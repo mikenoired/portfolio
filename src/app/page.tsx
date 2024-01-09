74c45ebfaf5a68779fe09472474cba92f7c27380
Mike Noired <mikevetkal@gmail.com>
2024-01-10 01:43:29 +0300

    Add place for video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import { Header } from './ui/header';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <div className='w-screen h-screen relative'>
+        <Image src='/video-blank.jpg' alt='Video preview' objectFit='cover' fill />
+      </div>
     </main>
-  )
+  );
 }
