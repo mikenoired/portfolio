ba4b06c8c58b0922495905040bc00c9d851d0a3e
Mike Noired <mikevetkal@gmail.com>
2024-01-10 02:05:28 +0300

    Change hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import Image from 'next/image';
-import { Header } from './ui/header';
+import Image from "next/image";
+import { Header } from "./ui/header";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header transparent={true} />
-      <div className='w-screen h-screen relative'>
-        <Image
-          src='/video-blank.jpg'
-          alt='Video preview'
-          objectFit='cover'
-          fill
-        />
-      </div>
-    </main>
+      <main>
+        <div className='w-screen h-screen relative'>
+          <Image
+            src='/video-blank.jpg'
+            alt='Video preview'
+            objectFit='cover'
+            fill
+          />
+        </div>
+      </main>
+    </>
   );
 }
