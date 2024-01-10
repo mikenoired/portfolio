3966a9bb69484da25cf7d9a963b0408d6fe9edd5
Mike Noired <mikevetkal@gmail.com>
2024-01-10 16:19:54 +0300

    Make start data template of works

---
+++ src/app/works/data.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    title: 'Studio',
+    thubnail: '/upload/street-thumb.png'
+  },
+  {
+    title: 'Street',
+    thubnail: '/upload/street-thumb.png'
+  },
+  {
+    title: 'Landscape',
+    thubnail: '/upload/street-thumb.png'
+  },
+  {
+    title: 'Print',
+    thubnail: '/upload/street-thumb.png'
+  }
+]
