cbb640ebdf9ac6ec769e7433a6523c9c21cb7469
Mike Noired <mikevetkal@gmail.com>
2024-01-10 15:20:43 +0300

    Add blocks for Q&A

--- src/app/qna/page.tsx
+++ src/app/qna/page.tsx
@@ -1,11 +1,17 @@
 import { Header } from "@/app/ui/header";
+import { blocks } from './data';
+import { Block } from '@/app/ui/qna/block';
 
 export default async function Page() {
   return (
     <>
       <Header transparent={false} />
-      <main className='md:pt-12'>
-        <h1>Q/A</h1>
+      <main className='md:pt-12 pt-10 flex flex-col items-center'>
+        <div className='masonry sm:masonry md:masonry-sm pt-8 md:max-w-[1050px] pl-4 pr-4'>
+          {blocks.map((block, index) => (
+            <Block key={index} data={block} />
+          ))}
+        </div>
       </main>
     </>
   );
---
+++ src/app/ui/qna/block.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+export const Block = ({data}: {data: {title: string, text: (string | string[])[]}}) => {
+  return <div
+    className='border border-white p-6 break-inside md:mb-8 mb-4'
+  >
+    <h1 className='text-4xl font-bold mb-3'>{data.title}</h1>
+    <div>
+      {data.text.map((p, index) => (
+        <div key={index} className='mb-5 last:mb-auto'>
+          <div className='text-xl font-semibold'>
+            {Array.isArray(p)
+              ? (p as string[]).map((listItem, index) => (
+                  <div key={index}>{listItem}</div>
+                ))
+              : p}
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+}
