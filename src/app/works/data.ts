3a036cadfe010075c51a4e6350b441d6627fbdf9
Mike Noired <mikevetkal@gmail.com>
2024-01-10 21:24:46 +0300

    Change temp data

--- src/app/works/data.ts
+++ src/app/works/data.ts
@@ -1,18 +1,106 @@
+export type Works = {
+  title: string,
+  url: string,
+  thumbnail: string,
+  images?: WorkImage[]
+}
+
+export type WorkImage = {
+  url: string,
+  caption: string
+}
+
 export const data = [
   {
     title: 'Studio',
-    thubnail: '/upload/street-thumb.png'
+    url: 'studio',
+    thumbnail: '/upload/street-thumb.png',
+    images: [
+      {
+        url: '/upload/1.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/2.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/3.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/4.jpg',
+        caption: '12441242'
+      }
+    ]
   },
   {
     title: 'Street',
-    thubnail: '/upload/street-thumb.png'
+    url: 'street',
+    thumbnail: '/upload/street-thumb.png',
+    images: [
+      {
+        url: '/upload/5.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/6.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/7.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/8.jpg',
+        caption: '12441242'
+      }
+    ]
   },
   {
     title: 'Landscape',
-    thubnail: '/upload/street-thumb.png'
+    url: 'land',
+    thumbnail: '/upload/street-thumb.png',
+    images: [
+      {
+        url: '/upload/9.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/10.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/11.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/1.jpg',
+        caption: '12441242'
+      }
+    ]
   },
   {
     title: 'Print',
-    thubnail: '/upload/street-thumb.png'
+    url: 'print',
+    thumbnail: '/upload/street-thumb.png',
+    images: [
+      {
+        url: '/upload/2.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/3.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/4.jpg',
+        caption: '12441242'
+      },
+      {
+        url: '/upload/5.jpg',
+        caption: '12441242'
+      }
+    ]
   }
 ]
