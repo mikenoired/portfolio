edfdd553ae4a75865ea2e740cea18190df58c3c2
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-01-12 01:21:05 +0300

    Move actions to spec file

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -3,6 +3,23 @@
 import { db } from '@/app/lib/utils';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { z } from "zod";
+
+const createQNASchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export async function newQNA(data: FormData) {
+  const createBlock = createQNASchema.parse({
+    title: data.get("title"),
+    content: data.get("content"),
+  });
+
+  const block = await db.answerBlock.create({
+    data: createBlock,
+  });
+}
 
 export async function deleteQNAById(currentId: number) {
   const action = await db.answerBlock.delete({
--- src/app/ui/admin/qna/BlockForm.tsx
+++ src/app/ui/admin/qna/BlockForm.tsx
@@ -1,24 +1,6 @@
-import { db } from "@/app/lib/utils";
-import { z } from "zod";
-
-const createQNASchema = z.object({
-  title: z.string(),
-  content: z.string(),
-});
+import { newQNA } from '@/app/lib/actions';
 
 export const BlockForm = () => {
-  async function newQNA(data: FormData) {
-    "use server";
-
-    const createBlock = createQNASchema.parse({
-      title: data.get("title"),
-      content: data.get("content"),
-    });
-
-    const block = await db.answerBlock.create({
-      data: createBlock,
-    });
-  }
   return (
     <div className='mt-5'>
       <h1 className='text-2xl font-bold mb-3'>Add new Q&A block</h1>
