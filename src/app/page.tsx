66c292441b2fe67ab2ce626e76ca28bed427baf8
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-01-30 02:01:29 +0300

    fix(index page): index thumbnail loading from server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
+import { fetchThumb } from "@/app/lib/actions";
+import { Header } from "@/app/ui/header";
 import Image from "next/image";
-import { Header } from "./ui/header";
 
-export default function Home() {
+export default async function Home() {
+  const thumb = await fetchThumb();
   return (
     <>
       <Header transparent={true} />
       <main>
         <div className='w-screen h-screen relative'>
           <Image
-            src='/upload/video-blank.jpg'
+            src={`/upload/${thumb?.media}`}
             alt='Video preview'
-            objectFit='cover'
+            className='object-cover'
             fill
           />
         </div>
