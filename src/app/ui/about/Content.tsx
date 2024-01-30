74430d3b92c5edddc06697c292b553e7df2acf54
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-01-31 02:32:57 +0300

    fix(about page): show content through markdown comp

--- src/app/ui/about/Content.tsx
+++ src/app/ui/about/Content.tsx
@@ -1,3 +1,9 @@
+import Markdown from "react-markdown";
+
 export default function Content({ data }: { data: string }) {
-  return <div>{data}</div>;
+  return (
+    <div className='prose-xl prose-ul:list-disc prose-ol:list-decimal pl-6'>
+      <Markdown>{data}</Markdown>
+    </div>
+  );
 }
