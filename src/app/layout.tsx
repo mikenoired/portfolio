048da89687bc944bb9f3fd8c6182a198fc9bbe77
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-01-30 03:59:03 +0300

    feat(grain): add grain effect on background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import grain from "@/app/lib/grain";
 import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
+import { MutableRefObject, useEffect, useRef } from "react";
 import "./globals.css";
 
 const archivo = Archivo({ subsets: ["latin"] });
@@ -14,10 +18,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const grainedCont = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => {
+    const options = {
+      animate: true,
+      patternWidth: 500,
+      patternHeight: 500,
+      grainOpacity: 0.2,
+      grainDensity: 1,
+      grainWidth: 0.5,
+      grainHeight: 0.5,
+      grainChaos: 0.2,
+      grainSpeed: 2,
+    };
+    grain(grainedCont.current, options);
+  });
   return (
     <html lang='en'>
       <body className={archivo.className + " bg-black text-white"}>
         {children}
+        <div
+          ref={grainedCont}
+          id='container'
+          className='w-dvw h-dvh fixed top-[0px] -z-50'
+        />
       </body>
     </html>
   );
---
+++ src/app/lib/grain.ts
@@ -0,0 +1,112 @@
+type OptionsType = {
+  animate: boolean;
+  patternWidth: number;
+  patternHeight: number;
+  grainOpacity: number;
+  grainDensity: number;
+  grainWidth: number;
+  grainHeight: number;
+  grainChaos: number;
+  grainSpeed: number;
+};
+
+export default function grain(ele: HTMLDivElement, options: OptionsType) {
+  const prefixes = ["", "-moz-", "-o-animation-", "-webkit-", "-ms-"];
+
+  const generateNoise = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = options.patternWidth;
+    canvas.height = options.patternHeight;
+    for (let w = 0; w < options.patternWidth; w += options.grainDensity) {
+      for (let h = 0; h < options.patternHeight; h += options.grainDensity) {
+        const rgb = (Math.random() * 256) | 0;
+        if (ctx) {
+          ctx.fillStyle = `rgba(${[
+            rgb,
+            rgb,
+            rgb,
+            options.grainOpacity,
+          ].join()})`;
+          ctx.fillRect(w, h, options.grainWidth, options.grainHeight);
+        }
+      }
+    }
+    return canvas.toDataURL("image/png");
+  };
+  const noise = generateNoise();
+
+  const addCSSRule = (
+    sheet: CSSStyleSheet,
+    selector: string,
+    rules: string
+  ) => {
+    let ins = "";
+    selector.length ? (ins = `${selector}{${rules}}`) : (ins = rules);
+
+    if ("insertRule" in sheet) {
+      sheet.insertRule(ins);
+    } else if ("addRule" in sheet) {
+      (sheet as CSSStyleSheet).insertRule(rules);
+    }
+  };
+
+  let animation = "";
+  const keyFrames = [
+    "0%:-10%,10%",
+    "10%:-25%,0%",
+    "20%:-30%,10%",
+    "30%:-30%,30%",
+    "40%::-20%,20%",
+    "50%:-15%,10%",
+    "60%:-20%,20%",
+    "70%:-5%,20%",
+    "80%:-25%,5%",
+    "90%:-30%,25%",
+    "100%:-10%,10%",
+  ];
+
+  let pre = prefixes.length;
+  while (pre--) {
+    animation += `@${prefixes[pre]}keyframes grained{`;
+    for (let key = 0; key < keyFrames.length; key++) {
+      const keyVal = keyFrames[key].split(":");
+      animation += `${keyVal[0]}{`;
+      animation += `${prefixes[pre]}transform:translate(${keyVal[1]});`;
+      animation += "}";
+    }
+    animation += "}";
+  }
+
+  const animationAdded = document.getElementById("graineAnimation");
+  if (animationAdded) {
+    animationAdded.parentElement?.removeChild(animationAdded);
+  }
+  let style = document.createElement("style");
+  style.id = "grained-animation";
+  style.innerHTML = animation;
+  document.body.appendChild(style);
+
+  const styleAdded = document.getElementById(`graineAnimation-"${ele.id}`);
+  if (styleAdded) {
+    styleAdded.parentElement?.removeChild(styleAdded);
+  }
+
+  style = document.createElement("style");
+  style.id = `graineAnimation-${ele.id}`;
+  document.body.appendChild(style);
+
+  let rule = `background-image:url(${noise});position:absolute;content:"";height:300%;width:300%;left:-100%;top:-100%;`;
+  pre = prefixes.length;
+  if (options.animate) {
+    while (pre--) {
+      rule += `${prefixes[pre]}animation-name:grained;`;
+      rule += `${prefixes[pre]}animation-iteration-count: infinite;`;
+      rule += `${prefixes[pre]}animation-duration:${options.grainChaos}s;`;
+      rule += `${prefixes[pre]}animation-timing-function: steps(${options.grainSpeed}, end);`;
+    }
+  }
+
+  const selectorElement = `#${ele.id}::before`;
+  addCSSRule(style.sheet as CSSStyleSheet, selectorElement, rule);
+}
