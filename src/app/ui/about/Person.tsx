796c572aa4c326fb1fbd479eb30b1293562d9e72
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-01-31 02:31:48 +0300

    feat(about page): load avatar from server

--- src/app/ui/about/Person.tsx
+++ src/app/ui/about/Person.tsx
@@ -1,11 +1,19 @@
 import { PersonType } from "@/app/lib/definitions";
 import Icon from "@/app/ui/Icon";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Person({ data }: { data: PersonType }) {
   return (
     <div className='max-w-[400px]'>
-      <div className='w-[300px] h-[300px] bg-white' />
+      <div className='w-[300px] h-[300px] relative'>
+        <Image
+          alt=''
+          fill
+          className='object-cover'
+          src={`/upload/${data.avatar}`}
+        />
+      </div>
       <div className='text-[28px] font-bold mt-3'>{data.name}</div>
       <div className='text-2xl font-semibold mt-3'>{data.job}</div>
       <div className='text-xl font-semibold mt-3'>{data.place}</div>
