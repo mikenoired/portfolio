e9955cb3d2c545e419cddb3ff4ed4f0a2ba94c18
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-01-31 16:32:52 +0300

    fix(home thumb form): disable upload if new image isn't selected

--- src/app/ui/admin/EditThumb.tsx
+++ src/app/ui/admin/EditThumb.tsx
@@ -11,9 +11,11 @@ export default function EditThumb({ thumb }: { thumb: ThumbType }) {
   const [image, setImage] = useState([""]);
   const [toggleManager, setToggleManager] = useState(false);
   const [preview, setPreview] = useState(thumb.media);
+  const [changed, isChanged] = useState(false);
   const saveHandler = (image: string[]) => {
     setImage(image);
     setPreview(image[0]);
+    isChanged(true);
   };
   return (
     <>
@@ -27,11 +29,18 @@ export default function EditThumb({ thumb }: { thumb: ThumbType }) {
       )}
       <div>
         <h1 className='font-semibold text-3xl mb-4'>Edit thumb</h1>
-        <form className='border p-6' action={updateThumb}>
+        <form
+          className='border p-6'
+          action={updateThumb}
+          onSubmit={() => isChanged(false)}
+        >
           <div
-            className='w-full relative min-h-[300px] mb-5 cursor-pointer'
+            className='w-full relative min-h-[300px] cursor-pointer'
             onClick={() => setToggleManager(true)}
           >
+            <div className='absolute flex text-2xl font-bold items-center justify-center w-full h-full bg-black z-[1] bg-opacity-0 opacity-0 hover:bg-opacity-60 hover:opacity-100'>
+              Change image
+            </div>
             <Image
               src={`/upload/${preview}`}
               fill
@@ -40,12 +49,14 @@ export default function EditThumb({ thumb }: { thumb: ThumbType }) {
             />
           </div>
           <input type='text' hidden name='url' value={image[0]} />
-          <button
-            className='w-full block p-4 border hover:bg-white hover:text-black font-medium text-lg'
-            type='submit'
-          >
-            Update
-          </button>
+          {changed && (
+            <button
+              className='mt-5 w-full block p-4 border hover:bg-white hover:text-black font-medium text-lg'
+              type='submit'
+            >
+              Update
+            </button>
+          )}
         </form>
       </div>
     </>
