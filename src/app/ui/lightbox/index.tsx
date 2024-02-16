8e5a15e7cb119b728d7b1f455b10bc9c94b6a56c
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-02-16 19:45:17 +0300

    feat(lightbox): make controlls showed on hover

--- src/app/ui/lightbox/index.tsx
+++ src/app/ui/lightbox/index.tsx
@@ -35,23 +35,23 @@ export default function Lightbox({
   return (
     <div className='w-full h-full fixed z-20 bg-black backdrop-blur-sm bg-opacity-50 top-[0px] left-[0px]'>
       <div className='w-full h-full absolute z-40 select-none'>
-        <div
-          onClick={toggleLightbox}
-          className='cursor-pointer absolute right-[20px] top-[20px]'
-        >
-          <Icon type='close' dark={false} width={25} height={25} />
+        <div className='absolute right-[20px] top-[20px] flex justify-end w-full opacity-0 hover:opacity-100 pb-[50px] z-[60]'>
+          <span className='right-6 relative font-medium text-lg'>
+            {urls.indexOf(current) + 1}/{urls.length}
+          </span>
+          <div onClick={toggleLightbox} className='cursor-pointer'>
+            <Icon type='close' dark={false} width={25} height={25} />
+          </div>
         </div>
-        <div
-          onClick={leftImage}
-          className='cursor-pointer absolute left-[20px] top-1/2'
-        >
-          <Icon type='back' dark={false} width={30} height={30} />
+        <div className='absolute opacity-0 pl-6 flex items-center hover:opacity-100 w-[350px] h-full'>
+          <div onClick={leftImage} className='cursor-pointer'>
+            <Icon type='back' dark={false} width={25} height={25} />
+          </div>
         </div>
-        <div
-          onClick={rightImage}
-          className='cursor-pointer absolute right-[20px] top-1/2'
-        >
-          <Icon type='right' dark={false} width={30} height={30} />
+        <div className='absolute right-[0px] pr-6 flex items-center justify-end opacity-0 hover:opacity-100 w-[350px] h-full'>
+          <div onClick={rightImage} className='cursor-pointer'>
+            <Icon type='right' dark={false} width={25} height={25} />
+          </div>
         </div>
       </div>
       <div className='w-screen h-screen z-30 absolute'>
