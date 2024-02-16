8a7327c183356838e1a8044185e0ece1f028cb25
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-02-16 21:14:05 +0300

    feat(lightbox): sliding animation

--- src/app/ui/lightbox/index.tsx
+++ src/app/ui/lightbox/index.tsx
@@ -3,7 +3,7 @@
 import { mobileSwipe } from "@/app/lib/mobileSwipe";
 import Icon from "@/app/ui/Icon";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Lightbox({
   urls,
@@ -14,6 +14,7 @@ export default function Lightbox({
   active: (toggle: boolean) => void;
   currentImage: string;
 }) {
+  const lightboxContainer = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleKeyPress = (e: KeyboardEvent) => {
       switch (e.key) {
@@ -45,28 +46,46 @@ export default function Lightbox({
   const [current, setCurrent] = useState<string>(currentImage);
 
   const leftImage = () => {
-    if (current == urls[0]) {
-      setCurrent(urls[urls.length - 1]);
-    } else {
-      setCurrent(urls[urls.indexOf(current) - 1]);
+    if (lightboxContainer.current) {
+      lightboxContainer.current.style.transform = `translateX(0%)`;
+      lightboxContainer.current.style.transition = "transform 0.3s ease-in-out";
     }
+    setTimeout(() => {
+      current == urls[0]
+        ? setCurrent(urls[urls.length - 1])
+        : setCurrent(urls[urls.indexOf(current) - 1]);
+      if (lightboxContainer.current) {
+        lightboxContainer.current.style.transition = "none";
+        lightboxContainer.current.style.transform = `translateX(-33.33333333333%)`;
+      }
+    }, 300);
   };
   const rightImage = () => {
-    if (current == urls[urls.length - 1]) {
-      setCurrent(urls[0]);
-    } else {
-      setCurrent(urls[urls.indexOf(current) + 1]);
+    if (lightboxContainer.current) {
+      lightboxContainer.current.style.transform = `translateX(-${
+        100 - 100 / 3
+      }%)`;
+      lightboxContainer.current.style.transition = "transform 0.3s ease-in-out";
     }
+    setTimeout(() => {
+      current == urls[urls.length - 1]
+        ? setCurrent(urls[0])
+        : setCurrent(urls[urls.indexOf(current) + 1]);
+      if (lightboxContainer.current) {
+        lightboxContainer.current.style.transition = "none";
+        lightboxContainer.current.style.transform = `translateX(-33.33333333333%)`;
+      }
+    }, 300);
   };
   return (
     <div className='w-full h-full fixed z-20 bg-black backdrop-blur-sm bg-opacity-50 top-[0px] left-[0px]'>
       <div className='w-full h-full absolute z-40 select-none'>
-        <div className='absolute right-[20px] top-[20px] flex justify-end w-full opacity-0 hover:opacity-100 pb-[50px] z-[60]'>
+        <div className='absolute right-[20px] top-[20px] flex justify-end items-center w-full opacity-0 hover:opacity-100 pb-[50px] z-[60]'>
           <span className='right-6 relative font-medium text-lg'>
             {urls.indexOf(current) + 1}/{urls.length}
           </span>
           <div onClick={toggleLightbox} className='cursor-pointer'>
-            <Icon type='close' dark={false} width={25} height={25} />
+            <Icon type='close' dark={false} width={20} height={20} />
           </div>
         </div>
         <div className='absolute opacity-0 pl-6 flex items-center hover:opacity-100 w-[350px] h-full'>
@@ -80,10 +99,40 @@ export default function Lightbox({
           </div>
         </div>
       </div>
-      <div className='w-screen h-screen z-30 absolute'>
-        <div className='w-full h-full'>
+      <div
+        className='w-[300%] h-screen z-30 absolute my-[0px] mx-auto flex'
+        style={{
+          transform: `translateX(-${100 / 3}%)`,
+        }}
+        ref={lightboxContainer}
+      >
+        <div className='w-1/3 h-full relative'>
+          <Image
+            src={`/upload/${
+              urls[urls.indexOf(current) - 1]
+                ? urls[urls.indexOf(current) - 1]
+                : urls[urls.length - 1]
+            }`}
+            alt=''
+            fill
+            className='object-contain'
+          />
+        </div>
+        <div className='w-1/3 h-full relative'>
           <Image
             src={`/upload/${current}`}
+            alt=''
+            fill
+            className='object-contain'
+          />
+        </div>
+        <div className='w-1/3 h-full relative'>
+          <Image
+            src={`/upload/${
+              urls[urls.indexOf(current) + 1]
+                ? urls[urls.indexOf(current) + 1]
+                : urls[0]
+            }`}
             alt=''
             fill
             className='object-contain'
