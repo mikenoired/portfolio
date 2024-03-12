9e8ec3efc630aa2529f3e30cd30080fa22357be1
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-12 23:50:17 +0300

    fix(file upload): make upload async

--- src/app/api/fileUpload/route.ts
+++ src/app/api/fileUpload/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request): Promise<NextResponse> {
   const data = await request.formData();
 
-  data.getAll("file").map((file) => loadMedia(file));
+  data.getAll("file").map(async (file) => await loadMedia(file));
 
   return NextResponse.json(data);
 }
