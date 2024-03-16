278861f3b859881ecdff06d3a451b007d5e34d81
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-16 03:31:32 +0300

    fix(works navigation): styles

--- src/app/ui/works/NavButton.tsx
+++ src/app/ui/works/NavButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/app/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
@@ -20,16 +21,21 @@ export function NavButton({
       onMouseEnter={(e) => setHover(true)}
       onMouseLeave={(e) => setHover(false)}
       href={`${pathname}/${url}`}
-      className="relative flex h-[90px] w-full items-center overflow-hidden border-y bg-cover bg-center bg-no-repeat transition-all xl:w-[1240px]"
+      className='relative flex h-[90px] w-full xl:w-[1240px] -mt-1 items-center overflow-hidden border-y bg-cover bg-center bg-no-repeat transition-all'
       style={{
         backgroundImage: hover ? `url('/upload/${thumb}')` : "none",
         right: hover ? "0px" : "-35px",
       }}
     >
-      <div className="z-[1] h-full px-8 text-[64px] font-bold mix-blend-difference">
+      <div className='z-[1] h-full px-8 text-[64px] font-bold mix-blend-difference'>
         {title}
       </div>
-      <div className="absolute h-[88px] w-full bg-black opacity-15"></div>
+      <div
+        className={cn(
+          "absolute h-[88px] w-full bg-black",
+          hover ? "opacity-15" : "opacity-0"
+        )}
+      ></div>
     </Link>
   );
 }
