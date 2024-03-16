86a600d4804b10c4904270c79adbb310ecc99364
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-16 03:06:19 +0300

    feat(link style): add style util for links

---
+++ src/app/ui/lib/linkStyle.ts
@@ -0,0 +1,14 @@
+export const linkStyle = [
+  "relative",
+  "pr-5",
+  'after:content-["\\0030"]',
+  "after:absolute",
+  "after:right-0",
+  "after:font-symlink",
+  "after:text-[50%]",
+  "after:ml-2",
+  "after:inline",
+  "after:mr-2",
+  "after:leading-snug",
+  "after:opacity-80",
+].join(" ");
