24db00c20f7b4de177f9bf28a4cc06ac4b3d6a3c
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-17 18:19:36 +0300

    feat(flow): add fetch and save data to form

--- src/app/(pages)/admin/flow/page.tsx
+++ src/app/(pages)/admin/flow/page.tsx
@@ -2,12 +2,17 @@
 
 import Main from "@/app/ui/Main";
 import EditPage from "@/app/ui/admin/flow/EditPage";
+import { fetchFlow } from "@/server/pages/flow";
 
 export default async function Page() {
+  const flow = await fetchFlow();
   return (
     <>
       <Main className='mt-12 flex justify-center'>
-        <EditPage images={[]} />
+        <EditPage
+          images={flow?.urls || []}
+          description={flow?.description || ""}
+        />
       </Main>
     </>
   );
--- src/app/ui/admin/flow/EditPage.tsx
+++ src/app/ui/admin/flow/EditPage.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import MediaManager from "@/app/ui/admin/MediaManager";
+import { updateFlow } from "@/server/pages/flow";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function EditPage({ images }: { images: string[] }) {
+export default function EditPage({
+  images,
+  description,
+}: {
+  images: string[];
+  description: string;
+}) {
   const [toggleManager, setToggleManager] = useState(false);
   const [flowImages, setImages] = useState(images);
+  const [flowDescription, setDescription] = useState(description);
+  const [changed, isChanged] = useState(false);
+  useEffect(() => {
+    if (images !== flowImages || description !== flowDescription) {
+      isChanged(true);
+    } else {
+      isChanged(false);
+    }
+  }, [images, flowImages, description, flowDescription]);
   return (
     <>
       {toggleManager && (
@@ -19,31 +35,56 @@ export default function EditPage({ images }: { images: string[] }) {
         />
       )}
       <div className='w-2/3 flex items-center flex-col'>
-        <div className='w-full'>
-          <h1 className='text-2xl font-semibold mb-3'>Description</h1>
-          <textarea className='min-h-[200px] w-full border bg-black bg-opacity-0 p-3' />
-        </div>
-        <div className='w-full mt-5'>
-          <button
-            type='button'
-            className='h-10 w-full bg-white text-black'
-            onClick={() => setToggleManager(true)}
-          >
-            Change images
-          </button>
-        </div>
+        <form className='w-full' action={updateFlow}>
+          {changed && (
+            <div className='w-full'>
+              <button className='h-10 w-full bg-white text-black' type='submit'>
+                Save
+              </button>
+            </div>
+          )}
+          <div className='w-full mt-5'>
+            <h1 className='text-2xl font-semibold mb-3'>Description</h1>
+            <textarea
+              name='description'
+              className='min-h-[200px] w-full border bg-black bg-opacity-0 p-3'
+              value={flowDescription}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder='What is this?'
+            />
+          </div>
+          <div className='w-full mt-5'>
+            <button
+              type='button'
+              className='h-10 w-full bg-white text-black'
+              onClick={() => setToggleManager(true)}
+            >
+              Change images
+            </button>
+            <input
+              type='text'
+              hidden
+              defaultValue={JSON.stringify(flowImages)}
+              name='urls'
+            />
+          </div>
+        </form>
         <div className='w-full mt-10'>
           <h1 className='text-2xl font-semibold mb-3'>flow.min</h1>
           <div className='flex flex-col items-center'>
-            {flowImages.map((image, index) => (
-              <Image
-                key={index}
-                src={`/upload/${image}`}
-                alt=''
-                width={500}
-                height={500}
-              />
-            ))}
+            {!!flowImages.length ? (
+              flowImages.map((image, index) => (
+                <Image
+                  key={index}
+                  src={`/upload/${image}`}
+                  alt=''
+                  width={500}
+                  height={500}
+                />
+              ))
+            ) : (
+              <p className='italic opacity-60'>There&apos;s too empty...</p>
+            )}
           </div>
         </div>
       </div>
