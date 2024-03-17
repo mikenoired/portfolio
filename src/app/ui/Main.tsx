0c0a77c760bef0b1d3526580a930a8eea06dc12c
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-17 18:57:59 +0300

    feat(main container): add param to remove margin by header

--- src/app/ui/Main.tsx
+++ src/app/ui/Main.tsx
@@ -1,12 +1,19 @@
-export default function Main({
-  children,
-  className,
-}: {
+import { cn } from "@/app/lib/utils";
+
+interface MainProps {
   children: React.ReactNode;
   className?: string;
-}) {
+  moveTop?: boolean;
+}
+
+export default function Main({ children, className, moveTop }: MainProps) {
   return (
-    <main className="absolute mt-10 h-screen w-screen overflow-y-scroll pb-10 md:mt-12 md:pb-12">
+    <main
+      className={cn(
+        "absolute mt-10 h-screen w-screen overflow-y-scroll pb-10 md:mt-12 md:pb-12",
+        moveTop && "mt-[0px] md:mt-[0px]"
+      )}
+    >
       <div className={className}>{children}</div>
     </main>
   );
