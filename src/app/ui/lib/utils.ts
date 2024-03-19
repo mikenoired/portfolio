c73162247690e3e3695e3ab7d772a632bc87f5ab
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-19 15:40:29 +0300

    refactor(utils)

--- src/app/ui/lib/utils.ts
+++ src/app/ui/lib/utils.ts
@@ -1,5 +1,5 @@
 const isTouchDevice = () => {
-  return "ontouchstart" in window || navigator.maxTouchPoints > 0;
+  return "ontouchstart" in window || !!navigator.maxTouchPoints;
 };
 
 export const isMobile = isTouchDevice();
