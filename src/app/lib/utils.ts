16d6c58b0483aa3165011673ac4f23a9ab288a82
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-23 01:16:30 +0300

    refactor(formatBytes): move function

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -14,6 +14,18 @@ export function formatDate(upd: string) {
   return format;
 }
 
+export function formatBytes(bytes: number, decimals = 2) {
+  if (!+bytes) return "0 B";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["B", "KiB", "MiB", "GiB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -9,15 +9,3 @@ declare global {
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 export default prisma;
-
-export function formatBytes(bytes: number, decimals = 2) {
-  if (!+bytes) return "0 B";
-
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["B", "KiB", "MiB", "GiB"];
-
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
-}
