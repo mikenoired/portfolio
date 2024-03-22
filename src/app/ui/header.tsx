f25ae09a30eccfda9eecb51d4da56ef08b863eb0
Mike Noired <43721293+mikenoired@users.noreply.github.com>
2024-03-23 01:17:59 +0300

    feat(header): change design & accessibility

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -7,24 +7,20 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useContext, useState } from "react";
 
-export default function Header({
-  transparent,
-  subMenu,
-  border,
-}: {
-  transparent: boolean;
-  subMenu: { title: string; url: string }[];
+interface HeaderProps {
   border?: boolean;
-}) {
+  invert?: boolean;
+}
+
+export default function Header({ border, invert }: HeaderProps) {
   const pathname = usePathname();
   const [toggleMenu, setToggleMenu] = useState(false);
-  const [toggleCat, setToggleCat] = useState(false);
   const names = useContext(HeaderContext);
   return (
     <header
       className={cn(
-        "absolute z-10 flex w-full flex-col mix-blend-difference contrast-100",
-        !transparent && "bg-black",
+        invert && "mix-blend-difference contrast-100",
+        "absolute z-10 flex w-full flex-col",
       )}
     >
       <div
@@ -44,7 +40,7 @@ export default function Header({
               className={cn(
                 "ml-5 hover:text-white uppercase",
                 pathname == `/${name.url}` &&
-                  "underline underline-offset-8 pointer-events-none",
+                  "underline underline-offset-2 pointer-events-none",
               )}
             >
               {name.name}
@@ -53,42 +49,32 @@ export default function Header({
         </nav>
         <div
           className="absolute right-8 md:hidden"
-          onClick={(e) => {
+          onClick={() => {
             setToggleMenu(!toggleMenu);
           }}
         >
-          {!toggleMenu ? (
-            <Icon type="menu" dark={false} width={20} height={20} />
-          ) : (
-            <Icon type="close" dark={false} width={20} height={20} />
-          )}
+          <Icon
+            type={!toggleMenu ? "menu" : "close"}
+            dark={false}
+            width={20}
+            height={20}
+          />
         </div>
       </div>
-      <div className={cn("flex-col", toggleMenu ? "flex" : "hidden")}>
-        <div className="flex w-full items-center border-b border-white pb-[14px] pl-8 pr-8  pt-[14px] md:h-12 md:justify-between">
-          <div
-            className="inline-flex items-center justify-center gap-3"
-            onClick={(e) => setToggleCat(!toggleCat)}
-          >
-            <div className="text-xl font-medium text-white">WORKS</div>
-            <Icon type="arrowDown" dark={false} width={12} height={12} />
-          </div>
-          <div className="pl-5 text-xl font-medium text-white">Q/A</div>
-          <div className="pl-5 text-xl font-medium">CONTACTS</div>
-        </div>
+      <div className={cn(toggleMenu ? "flex" : "hidden", "-mt-1")}>
         <div
           className={cn(
-            "w-full flex-wrap items-center border-b border-white pb-[14px] pl-8 pr-8 pt-[14px] md:justify-between",
-            toggleCat ? "flex" : "hidden",
+            !invert && "bg-black",
+            "w-full border-y border-white pb-[14px] px-8 pt-[12px] md:justify-between",
           )}
         >
-          {subMenu.map((link, index) => (
+          {names?.map((name, index) => (
             <Link
-              className="pr-5 text-xl font-medium uppercase"
-              href={`works/${link.url}`}
               key={index}
+              href={`/${name.url}`}
+              className="pr-5 pt-2 text-xl font-medium uppercase inline-block"
             >
-              {link.title}
+              {name.name}
             </Link>
           ))}
         </div>
